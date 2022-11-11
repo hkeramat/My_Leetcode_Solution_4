e1a2fa08d4f1fd358994be6af3d3fc97f6f6def9
Hadi Keramati <37190613+hkeramat@users.noreply.github.com>
2022-11-11 09:35:18 -0500

    Create 12. Integer to Roman.py

---
+++ 12. Integer to Roman.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), 
+                  (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), 
+                  (5, "V"), (4, "IV"), (1, "I")]
+        
+        ans = ''
+        
+        for value, key in digits:
+            cur_key_count, num = divmod(num,value)
+            ans += cur_key_count * key
+        return ans
